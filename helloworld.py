bcc8a79ca5d4f13716a7110ab1d8427b43dfada2
Hanna Manner <hanna.k.manner@gmail.com>
2022-10-06 16:24:19 +0300

    Wrote some code today
    I added a comment in the middle specifically for you to see. Was wondering about a feature, and thought you might know how to do it.

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,19 @@
-print("Hello, World!")
+
+y = "Good day!"
+print(y)
+x = input("How many customers in line? ")
+if x>1:
+    print("Please take a seat!")
+
+"""
+How would you go about putting a pause here, like it takes a while for a 
+patient to get called to the pharmacist desk?
+"""
+
+prescription = input("Do you have a prescription?: ")
+patient = input("Status of patient: ")
+
+if prescription == "Yes" and patient == "alive":
+    print("New prescription") 
+else:
+    print("No prescription")
