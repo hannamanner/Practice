27ebc2b83111df0c444ff040483ddbf907ac1f40
Hanna Manner <hanna.k.manner@gmail.com>
2022-10-06 18:30:08 +0300

    New and improved, I guess

--- helloworld.py
+++ helloworld.py
@@ -1,13 +1,16 @@
 
 y = "Good day!"
 print(y)
-x = input("How many customers in line? ")
-x_2 = int(x)
-if x_2 > 1:
+x = int(input("How many customers in line? "))
+if x > 1:
     print("Please take a seat!")
 print("Next, please!")
 
-prescription = input("Do you have a prescription?: ")
+prescription = input("Do you have a prescription? (Yes/No): ")
+
+if prescription == "No" or prescription == "no":
+    print("I'm sorry, I can't help you then.")
+    quit()
 patient = input("Status of patient: ")
 
 if prescription == "Yes" and patient == "alive":
