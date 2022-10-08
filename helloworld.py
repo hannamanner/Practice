325fda411424173a9e9e750140fa85fd32c3a648
Hanna Manner <hanna.k.manner@gmail.com>
2022-10-08 14:14:28 +0300

    I think I'm done with this

--- helloworld.py
+++ helloworld.py
@@ -1,20 +1,38 @@
 import time
-y = "Good day!"
-print(y)
-x = int(input("How many customers in line? "))
+print("Good day!")
+
+while True:
+    
+    try:
+        x = int(input("How many customers in line? "))
+    except ValueError:
+        print("... My dude.")
+        continue
+    if x <1 or x > 20:
+        print("There's no way!")
+    else:
+        break
+
 if x > 1:
     print("Please take a seat!")
-time.sleep(10)
+    doze = 5
+    x = x-1
+while x > 2:
+    x = x-1
+    time.sleep(doze)
+    print(f"Just a bit longer, only {x} more people before you...")
+time.sleep(doze)
+print(f"Just a bit longer, only 1 more person before you...")
+time.sleep(doze)
 print("Next, please!")
 
 prescription = input("Do you have a prescription? (Yes/No): ")
-
-if prescription == "No" or prescription == "no":
+if prescription.lower() != "yes":
     print("I'm sorry, I can't help you then.")
     quit()
 patient = input("Status of patient: ")
 
-if prescription == "Yes" and patient == "alive":
-    print("New prescription") 
+if patient.lower() == "alive":
+    print("Here's your prescription, have a nice day!") 
 else:
-    print("No prescription")
+    print("What on Earth are you doing here then?!")
